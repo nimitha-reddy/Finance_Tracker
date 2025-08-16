e3fb0d678fa78369cbeca6acf5884a1aa981b55b
Nimitha Singireddy <singireddynimitha07@gmail.com>
2025-08-16 20:21:38 +0530

    Move Firebase config to .env file

--- src/firebase.js
+++ src/firebase.js
@@ -10,12 +10,12 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnD6tDL95YFlFlcyFiwKcSBHQFf8q7FKE",
-  authDomain: "financly-69668.firebaseapp.com",
-  projectId: "financly-69668",
-  storageBucket: "financly-69668.appspot.com",
-  messagingSenderId: "288783895041",
-  appId: "1:288783895041:web:bab032e0c301a673c88e00",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
   measurementId: "G-K8RXNNWQJC"
 };
 
